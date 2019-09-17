30a5424d561dd23fd9b79c0fbcc5ef74a6d377df
xinyongpeng <1365155759@qq.com>
2019-09-17 14:57:39 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <fstream>
+
+using namespace  std;
+int main(char argc, char *argv[])
+{
+	// FILE* f = fopen("test", "r");
+	// string data;
+	// fscanf(f, "%s", data);
+	string data;
+	ifstream infile;
+	infile.open("test");
+	if(!infile.is_open())
+	{
+		cout << "打开文件错误！！" << endl;
+	}
+	infile >> data;
+
+}
